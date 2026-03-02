886cc22b3cbc91fa9d7232254fe17bbc473d55ce
UshaRapireddy <rapireddy.usha@capsitech.com>
2026-03-02 20:03:22 +0530

    Info panel added

--- Assets/Scripts/Controllers/SettingController.cs
+++ Assets/Scripts/Controllers/SettingController.cs
@@ -3,6 +3,18 @@ using UnityEngine;
 public class SettingController : MonoBehaviour
 {
     public GameObject settingsPanel;
+
+    public GameObject infoPanel;
+
+    public void OpenInfo()
+    {
+        infoPanel.SetActive(true);
+    }
+
+    public void CloseInfo()
+    {
+        infoPanel.SetActive(false);
+    }
     public void OpenSettings()
     {
         settingsPanel.SetActive(true);
